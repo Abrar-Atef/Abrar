250ef8cd3a6e670ffdc3169a6f9078a2c44d719b
Abrar-Atef <abraratef777@gmail.com>
2022-04-07 05:45:35 +0200

    modifying

---
+++ task1/Even&Odd/index.php
@@ -0,0 +1,54 @@
+<?php
+if ($_POST) {
+  if($_POST['Enter_Num']%2==0) {
+      $message =
+        "<div class='alert alert-success'> 
+         Even Number
+      </div>";
+  } else{
+    $message =
+      "<div class='alert alert-success'> 
+        Odd Number
+    </div>";
+  }
+}
+
+?>
+
+<!---------------------------Design--------------------------->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Even&Odd</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <script src="js/bootstrap.bundle.js"></script>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center my-5">
+        <h4 class="fs-2 text-primary">Even & Odd Number</h4>
+      </div>
+      <div class="col-4 offset-4">
+        <form method="post">
+          <div class="mb-3">
+            <input type="number" class="form-control" id="Num" name="Enter_Num" placeholder=" Enter Number">
+          </div>
+          <button class="btn btn-primary">Calc</button>
+        </form>
+        <?php 
+        if (isset($message)) {
+          echo $message ;
+        }
+        ?>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ task1/Max&Min/index.php
@@ -0,0 +1,96 @@
+<?php
+if ($_POST) {
+  if($_POST['First_Num'] > $_POST['Second_Num'] &&  $_POST['Second_Num'] > $_POST['Third_Num'] ) {
+      $message =
+        "<div class='alert alert-success'> 
+         Max Number is first Number 
+         <br>
+         Min Number is Third Number 
+      </div>";
+  }elseif($_POST['First_Num'] > $_POST['Third_Num'] &&  $_POST['Third_Num'] > $_POST['Second_Num'] ) {
+    $message =
+      "<div class='alert alert-success'> 
+       Max Number is first Number 
+       <br>
+       Min Number is Second Number 
+    </div>";
+}elseif($_POST['Second_Num'] > $_POST['First_Num'] &&  $_POST['First_Num'] > $_POST['Third_Num'] ) {
+  $message =
+    "<div class='alert alert-success'> 
+     Max Number is Second Number 
+     <br>
+     Min Number is Third Number 
+  </div>";
+}elseif($_POST['Second_Num'] > $_POST['Third_Num'] &&  $_POST['Third_Num'] > $_POST['First_Num'] ) {
+  $message =
+    "<div class='alert alert-success'> 
+     Max Number is Second Number 
+     <br>
+     Min Number is First Number 
+  </div>";
+}elseif($_POST['Third_Num'] > $_POST['First_Num'] &&  $_POST['First_Num'] > $_POST['Second_Num'] ) {
+  $message =
+    "<div class='alert alert-success'> 
+     Max Number is Third Number 
+     <br>
+     Min Number is Second Number 
+  </div>";
+}elseif($_POST['Third_Num'] > $_POST['Second_Num'] &&  $_POST['Second_Num'] > $_POST['First_Num'] ) {
+  $message =
+    "<div class='alert alert-success'> 
+     Max Number is Third Number 
+     <br>
+     Min Number is First Number 
+  </div>";
+}else{
+  "<div class='alert alert-danger'> 
+     Their is more than one value selected  
+</div>";
+}     
+}
+
+?>
+
+<!---------------------------Design--------------------------->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Even&Odd</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <script src="js/bootstrap.bundle.js"></script>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center my-5">
+        <h4 class="fs-2 text-primary">Max & Min Number</h4>
+      </div>
+      <div class="col-4 offset-4">
+        <form method="post">
+          <div class="mb-3">
+            <input type="number" class="form-control" id="Num" name="First_Num" placeholder=" Enter First Number">
+          </div>
+          <div class="mb-3">
+            <input type="number" class="form-control" id="Num" name="Second_Num" placeholder=" Enter Second Number">
+          </div>
+          <div class="mb-3">
+            <input type="number" class="form-control" id="Num" name="Third_Num" placeholder=" Enter Third Number">
+          </div>
+          <button class="btn btn-primary">Calc</button>
+        </form>
+        <?php 
+        if (isset($message)) {
+          echo $message ;
+        }
+        ?>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
